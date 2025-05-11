adde9650549cddb84e45ec10f1b0c2fdbd63b14d
jieun <yudaeun222@gmail.com>
2025-05-11 18:42:33 +0900

    exception

--- java-mid1/java-mid1/src/exception/ex4/MainV4.java
+++ java-mid1/java-mid1/src/exception/ex4/MainV4.java
@@ -34,7 +34,7 @@ public class MainV4 {
         System.out.println("사용자 메시지: 죄송합니다. 알 수 없는 문제가 발생했습니다.");
         System.out.println("==개발자용 디버깅 메시지==");
         e.printStackTrace(System.out); //스택 트레이스 출력
-        //e.printStackTrace();
+//        e.printStackTrace();
 
         //필요하면 예외 별로 별도의 추가 처리 가능
         if (e instanceof SendExceptionV4 sendEx) {
