d71673a11c8c5d4fee84c46e17ec5f421a0e783c
jieun <yudaeun222@gmail.com>
2025-06-03 11:39:49 +0900

    spring

---
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -0,0 +1,34 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
